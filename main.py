2d6b46eab664ff19a2d859ea7759b900c34a93d5
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-07 16:00:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.title('Hai !')
 siapa = ['yari', 'bukann yari', 'kesayangan juju', 'arip']
 nama = st.selectbox('ini saha ya ? ', siapa) 
 if nama=='yari' :
   st.write('cih polosan amat yari doang :P')
+  elif nama=='bukann yari' :
+    st.write('pasti mba kunti')

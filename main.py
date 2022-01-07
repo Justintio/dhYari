540b8c9545c0ad37af42c3c7764088cf001d9cf4
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-07 15:56:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title('Hai !')
 siapa = ['yari', 'bukann yari', 'kesayangan juju', 'arip']
 nama = st.selectbox('ini saha ya ? ', siapa) 
-if nama=='yari'
+if nama=='yari' :
   st.write('cih polosan amat yari doang :P')

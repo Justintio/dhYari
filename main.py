04848c6445c43ba53e895b0e36ae59676a54c408
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-07 16:01:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ siapa = ['yari', 'bukann yari', 'kesayangan juju', 'arip']
 nama = st.selectbox('ini saha ya ? ', siapa) 
 if nama=='yari' :
   st.write('cih polosan amat yari doang :P')
-  elif nama=='bukann yari' :
-    st.write('pasti mba kunti')
+elif nama=='bukann yari' :
+  st.write('pasti mba kunti')

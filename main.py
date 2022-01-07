bc42fab9bead765d1ac8f9aa2e1d5310e614f2d1
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-07 16:04:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from PIL import Image
 
-left_col, mid_col, right_col = st.columns(3)
+
 st.title('Hai !')
 siapa = ['yari', 'bukann yari', 'kesayangan juju', 'arip']
 nama = st.selectbox('ini saha ya ? ', siapa) 
+
+left_col, mid_col, right_col = st.columns(3)
 if nama=='yari' :
   mid_col.write('cih polosan amat yari doang :P')
 elif nama=='bukann yari' :

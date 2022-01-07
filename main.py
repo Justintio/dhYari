62ff705f4fa3b362fd89f34738838c5fd015115f
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-07 16:15:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ if nama=='kosong' :
   left_col.write(' ')
 if nama=='yari' :
   st.title('cih polosan amat yari doang :P')
+  lanjut = ['lanjut', 'ga']
+  lanjut2 = st.selectbox('ini saha ya ? ', lanjut) 
+  if lanjut2 == 'ga' :
+    st.title('yaudin')
+  if lanjut2 == 'lanjut' :
+    st.title('a')
+    st.title('b')
+    st.title('c')
+    st.title('d')
+    st.title('e')
+    st.title('f')
+    st.title('g')
+    st.title('h')
+    st.title('i lop u veri veri much')
+    st.title('hahaha dah coba yang lain sana !')
 elif nama=='bukann yari' :
   st.title('pasti mba kunti')
